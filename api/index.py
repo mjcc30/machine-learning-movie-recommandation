0ca6b674652be4faabb065568d0ba9f132e8db96
mjcc30 <m.cordeiro@ecole-ipssi.net>
2023-06-14 22:11:03 +0000

    fix index not working

--- api/index.py
+++ api/index.py
@@ -1,1 +1,12 @@
 from wsgi import app
+
+PORT = 5100
+DEV = False
+
+# Run app
+if (DEV):
+    app.run(debug=True, port=5050)
+
+if __name__ == '__main__':
+    print("Api running on port : {} ".format(PORT))
+    app.run(host="0.0.0.0", port=PORT)
--- api/wsgi.py
+++ api/wsgi.py
@@ -1,5 +1,22 @@
-# Api prod mode
-from main import app
+from flask import Flask,request
+from routes.api import create_routes
 
-if __name__ == "__main__":
-    app.run()
+# Initialize flask
+app = Flask(__name__)
+
+# root for vercel healthcheck
+@app.route("/")
+def home():
+    return "HELLO from vercel use flask"
+
+# Set headers json for all requests
+@app.after_request
+def apply_caching(response):
+    response.headers["Content-Type"] = "application/json"
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Headers'] = '*'
+    response.headers['Upgrade-Insecure-Requests'] = 1
+    return response
+
+# Import routes
+create_routes(app)
